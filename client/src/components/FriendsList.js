83343c5a14bce12ab45e781243b50c7e9f6dab92
Ali <ar.k@outlook.com>
2018-04-10 10:19:59 -0700

    Completed

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class FriendsList extends Component {
+  constructor() {
+    super()
+  
+    this.state = {
+      friends: []
+    }
+  }
+  
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+      .then((response) => {
+        this.setState({
+          friends: response.data
+        })
+      })
+  }
+      
   render() {
     return (
       <div>
